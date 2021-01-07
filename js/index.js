959c592673744039d7c3542ff71487564ea552fe
Erfane Faramarzi <erf.frz@gmail.com>
2021-01-07 13:15:16 +1100

    first commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+window.addEventListener('load', ()=>{
+    const preload = document.querySelector('.preload');
+    preload.classList.add('preload-finish');
+});
+
+
+
+//in order to show the preload page, hed to the console => network -> online -> slow 3G
+//then right click on the reload button of the page and select empty cache and hard reload
